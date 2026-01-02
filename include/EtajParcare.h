aeb83cbd5bc97164fff22f5012b8fa05420bf0f3
LuciferSan-byte <apostolescu.alexandru233@gmail.com>
2026-01-02 15:15:37 +0200

    implementarea etajparcare

--- include/EtajParcare.h
+++ include/EtajParcare.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <vector>
-
+#include <iostream>
 class LocParcare;
 class Vehicul;
 
@@ -14,7 +14,14 @@ public:
     EtajParcare(int numarEtaj);
 
     void adaugaLoc(LocParcare* loc);
+    
     bool areLocLiber(const Vehicul& v) const;
-    LocParcare* obtineLocLiber(const Vehicul& v) const;
+    int  numarLocuriLibere() const;
+
+    bool parcheazaPeLoc(int idLoc, const Vehicul &v) const;
+    bool parcheazaAutomat(const Vehicul &v) const;
+    
+    void nrLocuriLiberePeCategorii() const;
+    void afiseazaStareEtaj() const;
 };
 
---
+++ src/EtajParcare.cpp
@@ -0,0 +1,136 @@
+#include "EtajParcare.h"
+#include "Vehicul.h"
+#include "LocParcare.h"
+
+EtajParcare::EtajParcare(int numarEtaj) : numarEtaj(numarEtaj){}
+
+void EtajParcare::adaugaLoc(LocParcare* loc){
+    if(!loc) return;
+    locuriParcare.push_back(loc);
+}
+
+
+bool EtajParcare::parcheazaPeLoc(int idLoc, const Vehicul & v) const{
+    for(auto& loc : locuriParcare) {
+	if(loc->obtineId() == idLoc){
+	    
+	    if(loc->esteOcupat()){
+		std::cout << "Locul " << loc->obtineId() << " este deja ocupat\n";
+		return false;
+	    }
+	
+	if(!loc->acceptaVehicul(v)){
+	    std::cout << "Vehiculul nu este acceptat pe locul " << loc->obtineId() << "\n";
+	    return false;
+	}
+
+	loc->ocupa();
+	std::cout << "Vehicul parcat pe locul ales: " << idLoc << "\n";
+	return true;
+	}
+    }
+
+    std::cout << "Locul nu exista \n";
+    return false;
+}
+
+bool EtajParcare::parcheazaAutomat(const Vehicul &v) const{
+    for(auto &loc : locuriParcare){
+	if(!loc->esteOcupat() && loc->acceptaVehicul(v)){
+	    loc->ocupa();
+	    std::cout << "Vehicul parcat automat pe locul "
+		      << loc->obtineId() << "\n";
+	    return true;
+	}
+    }
+    std::cout << "Nu s-a gasit un loc potrivit \n";
+    return false;
+}
+
+bool EtajParcare::areLocLiber(const Vehicul & v) const{
+    for(const auto & loc : locuriParcare){
+	if(!loc->esteOcupat() && loc->acceptaVehicul(v)){
+	    return true;
+	}
+    }
+    return false;
+}
+
+int EtajParcare::numarLocuriLibere() const{
+    int count = 0;
+    for(const auto& loc : locuriParcare){
+	if(!loc->esteOcupat()){
+	    count++;
+	}
+    }
+    return count;
+}
+
+
+
+
+void EtajParcare::nrLocuriLiberePeCategorii() const{
+    int nrLocuriElectric = 0;
+    int nrLocuriMasina = 0;
+    int nrLocuriMoto = 0;
+    int nrLocuriCamion = 0;
+    int nrLocuriHandicap = 0;
+    int nrLocuriHandicapElectric = 0;
+
+    std::cout << "=== Etaj " << numarEtaj << " ===\n";
+
+    for (const auto& loc : locuriParcare) {
+	if(!loc->esteOcupat()){
+        std::string tip = loc->obtineVehiculAcceptat();
+
+        if (tip == "Masina") {
+            if (loc->esteHandicap()) {
+                if (loc->esteElectric())
+                    nrLocuriHandicapElectric++;
+                else
+                    nrLocuriHandicap++;
+            } else {
+                if (loc->esteElectric())
+                    nrLocuriElectric++;
+                else
+                    nrLocuriMasina++;
+            }
+        }
+        else if (tip == "Motocicleta") {
+            nrLocuriMoto++;
+        }
+        else if (tip == "Camion") {
+            nrLocuriCamion++;
+        }
+    }
+    }
+    std::cout << "Locuri libere: " << numarLocuriLibere() << "\n\n";
+    std::cout << "Nr locuri masina: " << nrLocuriMasina << "\n"
+              << "Nr locuri electric: " << nrLocuriElectric << "\n"
+              << "Nr locuri handicap: " << nrLocuriHandicap << "\n"
+              << "Nr locuri handicap electrice: " << nrLocuriHandicapElectric << "\n"
+              << "Nr locuri moto: " << nrLocuriMoto << "\n"
+              << "Nr locuri camion: " << nrLocuriCamion << "\n";
+}
+
+void EtajParcare::afiseazaStareEtaj() const{
+    std::cout << "=== Etaj " << numarEtaj << " ===\n";
+   
+    for(auto & loc : locuriParcare){
+	loc->afiseazaStare();
+	
+	std::cout << "Tip loc: " << loc->obtineVehiculAcceptat() << "\n";
+	
+	std::cout << "Electric: ";
+	std::cout << (loc->esteElectric() ? "Da" : "Nu");
+	std::cout << "\n";
+
+	if(loc->obtineVehiculAcceptat() == "Masina"){
+	    std::cout << "Loc pentru persoane cu handicap: ";
+	    std::cout << (loc->esteHandicap() ? "Da" : "Nu");
+	    std::cout << "\n";
+	}
+
+	std::cout << "\n\n-----------------\n\n";
+	}
+    }
